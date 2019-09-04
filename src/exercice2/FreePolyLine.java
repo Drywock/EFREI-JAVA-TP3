666b1e26c3b64c4a2e85b0ee5f8deb9f02541f54
Thomas LINTANF <thomas.lintanf@efrei.net>
2019-09-04 11:40:56 +0200

    Finalizing exercice 2

---
+++ src/exercice2/FreePolyLine.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package exercice2;
+
+/**
+ * @author Thomas LINTANF
+ *
+ */
+
+import exercice1.Point;
+
+public class FreePolyLine {
+	private java.util.LinkedList<Point> m_points;
+	
+	public FreePolyLine() {
+		m_points = new java.util.LinkedList<Point>();
+	}
+	
+	public void add(Point pt) {
+		m_points.add(pt);
+	}
+	
+	public int pointCount() {
+		return m_points.size();
+	}
+	
+	public boolean contains(Point pt) {
+		return m_points.contains(pt);
+	}
+	
+	//Question 2 
+	// the method pointCapacity will not be implemented because their is no maximum number of points
+}
